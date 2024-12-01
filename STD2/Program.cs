fb77d9e26d459a1ab6c0d572e837d5f37bca49cf
maior <maior@DESKTOP-FHGLER9>
2024-12-01 18:57:21 +0200

    Добавьте файлы проекта.

---
+++ STD2/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+
+        Console.WriteLine("Введите строку:");
+        string input = Console.ReadLine();
+        string result = Regex.Replace(input, "[a-zA-Z]", "");
+        Console.WriteLine(result);
+    
